3d43b05ab2f083daf2eb29c6a45281ad5a94a1eb
Lan Dam <ldam@passcal.nmt.edu>
2019-02-27 11:41:23 -0700

    #128 remove global variables by changing to class

--- ph5/utilities/pformacl.py
+++ ph5/utilities/pformacl.py
@@ -12,68 +12,8 @@ import logging
 from multiprocessing import cpu_count
 from ph5.utilities import pforma_io
 
-PROG_VERSION = '2019.043'
-logging.basicConfig()
+PROG_VERSION = '2019.058'
 LOGGER = logging.getLogger(__name__)
-
-
-def get_args():
-    global JSON_DB, JSON_CFG, RAW_LST, PROJECT, NFAMILY, M, MERGE, UTM, TSPF
-
-    parser = argparse.ArgumentParser(
-                                formatter_class=argparse.RawTextHelpFormatter)
-
-    parser.usage = ("pforma --project_home=/path/to/project "
-                    "--files=list_of_raw_files [options] | "
-                    "--project_home=/path/to/project --merge")
-    parser.description = ("Create or open a project and process raw data "
-                          "to PH5 in parallel.\n\nVersion: {0}"
-                          .format(PROG_VERSION))
-
-    parser.add_argument("-f", "--files", dest="infile",
-                        help="File containing list of raw file names.",
-                        metavar="file_list_file")
-    parser.add_argument("-p", "--project_home", dest="home",
-                        help="Path to project directory.", default=".")
-    parser.add_argument("-n", "--num_families", dest="nfamilies",
-                        help="Number of PH5 families to process. Defaults to "
-                             "number of CPU's + 1 else number of raw files.",
-                        type=int)
-    parser.add_argument("-M", "--num_minis", dest="num_minis",
-                        help="Number of mini ph5 files per family.",
-                        type=int)
-    parser.add_argument("-U", dest="utm",
-                        help="The UTM zone if required for SEG-D conversion.",
-                        type=int)
-    parser.add_argument("-T", "--TSPF", dest="tspf",
-                        help="Coordinates is texas state plane coordinates "
-                             "(SEG-D).",
-                        action="store_true", default=False)
-    parser.add_argument("-m", "--merge", dest="merge_minis",
-                        help="Merge all families to one royal family in A.",
-                        action="store_true", default=False)
-
-    args = parser.parse_args()
-
-    if args.infile and args.merge_minis:
-        raise Exception("Loading and merging must be done as seperate "
-                        "operations. Exiting.")
-
-    if args.infile is not None and not os.path.exists(args.infile):
-        raise Exception("Can not find {0}.".format(args.infile))
-
-    else:
-        RAW_LST = args.infile
-
-    if args.nfamilies is None:
-        NFAMILY = cpu_count() + 1
-    else:
-        NFAMILY = args.nfamilies
-    PROJECT = args.home
-    M = args.num_minis
-    UTM = args.utm
-    TSPF = args.tspf
-    MERGE = args.merge_minis
 
 
 def exexists(exe):
@@ -84,96 +24,148 @@ def exexists(exe):
         return False
 
 
-def adjust(fio):
-    if fio.number_raw <= len(fio.nmini):
-        print "The number of files is small compared to number of families!"
-        yn = raw_input("Adjust number of families and M within family? (y/n) ")
-        if yn == 'y':
-            fio.set_nmini(fio.number_raw)
-            fio.set_M(1)
+class Pformacl():
+    def get_args(self):
+        parser = argparse.ArgumentParser(
+            formatter_class=argparse.RawTextHelpFormatter)
 
+        parser.usage = ("pforma --project_home=/path/to/project "
+                        "--files=list_of_raw_files [options] | "
+                        "--project_home=/path/to/project --merge")
+        parser.description = ("Create or open a project and process raw data "
+                              "to PH5 in parallel.\n\nVersion: {0}"
+                              .format(PROG_VERSION))
+        parser.add_argument(
+            "-f", "--files", dest="infile", metavar="file_list_file",
+            help="File containing list of raw file names.")
+        parser.add_argument(
+            "-p", "--project_home", dest="home",
+            help="Path to project directory.", default=".")
+        parser.add_argument(
+            "-n", "--num_families", dest="nfamilies", type=int,
+            help="Number of PH5 families to process. Defaults to "
+            "number of CPU's + 1 else number of raw files.")
+        parser.add_argument(
+            "-M", "--num_minis", dest="num_minis", type=int,
+            help="Number of mini ph5 files per family.")
+        parser.add_argument(
+            "-U", dest="utm", type=int,
+            help="The UTM zone if required for SEG-D conversion.")
+        parser.add_argument(
+            "-T", "--TSPF", dest="tspf", action="store_true", default=False,
+            help="Coordinates is texas state plane coordinates (SEG-D).")
+        parser.add_argument(
+            "-m", "--merge", dest="merge_minis", action="store_true",
+            default=False, help="Merge all families to one royal family in A.")
 
-def run(fio):
-    import subprocess
-    #
-    # cmds -> cmds[Family] => List of commands to execute
-    # pees -> pees[Family] => Subprocess
-    # i -> The index of the list of commands that is running
-    # XXX   Debug
-    cmds, pees, i = fio.run()
+        args = parser.parse_args()
 
-    ll = {'A': i, 'B': i, 'C': i, 'D': i, 'E': i, 'F': i, 'G': i, 'H': i,
-          'I': i, 'J': i, 'K': i, 'L': i, 'M': i, 'N': i, 'O': i, 'P': i}
-    out = {}
-    fifo = {}
-    xterms = {}
-    running = True
-    for m in fio.nmini:
-        fifo[m] = "/tmp/fifo{0}".format(m)
-        if not os.path.exists(fifo[m]):
-            os.mkfifo(fifo[m])
+        if args.infile and args.merge_minis:
+            raise Exception("Loading and merging must be done as seperate "
+                            "operations. Exiting.")
 
-        xterms[m] = subprocess.Popen(
-            ['xterm', '-geometry', '80X3', '-T', m, '-e', 'tail', '-f',
-             fifo[m]])
+        if args.infile is not None and not os.path.exists(args.infile):
+            raise Exception("Can not find {0}.".format(args.infile))
 
-    while running:
-        running = False
+        else:
+            self.RAW_LST = args.infile
+
+        if args.nfamilies is None:
+            self.NFAMILY = cpu_count() + 1
+        else:
+            self.NFAMILY = args.nfamilies
+        self.PROJECT = args.home
+        self.M = args.num_minis
+        self.UTM = args.utm
+        self.TSPF = args.tspf
+        self.MERGE = args.merge_minis
+
+    def adjust(self, fio):
+        if fio.number_raw <= len(fio.nmini):
+            print "The number of files is small compared to number of " + \
+                  "families!"
+            yn = raw_input(
+                "Adjust number of families and M within family? (y/n) ")
+            if yn == 'y':
+                fio.set_nmini(fio.number_raw)
+                fio.set_M(1)
+
+    def run(self, fio):
+        import subprocess
+        #
+        # cmds -> cmds[Family] => List of commands to execute
+        # pees -> pees[Family] => Subprocess
+        # i -> The index of the list of commands that is running
+        # XXX   Debug
+        cmds, pees, i = fio.run()
+
+        ll = {'A': i, 'B': i, 'C': i, 'D': i, 'E': i, 'F': i, 'G': i, 'H': i,
+              'I': i, 'J': i, 'K': i, 'L': i, 'M': i, 'N': i, 'O': i, 'P': i}
+        out = {}
+        fifo = {}
+        xterms = {}
+        running = True
         for m in fio.nmini:
-            if pees[m] is None:
-                continue
-            if pees[m].poll() == 0:
-                ll[m] += 1
-                t, ll[m] = fio.run_cmds(cmds, x=ll[m], ems=m)
-                if t is not None:
-                    pees[m] = t
-            if pees[m].poll() is None:
-                running = True
+            fifo[m] = "/tmp/fifo{0}".format(m)
+            if not os.path.exists(fifo[m]):
+                os.mkfifo(fifo[m])
+
+            xterms[m] = subprocess.Popen(
+                ['xterm', '-geometry', '80X3', '-T', m, '-e', 'tail', '-f',
+                 fifo[m]])
+
+        while running:
+            running = False
+            for m in fio.nmini:
+                if pees[m] is None:
+                    continue
+                if pees[m].poll() == 0:
+                    ll[m] += 1
+                    t, ll[m] = fio.run_cmds(cmds, x=ll[m], ems=m)
+                    if t is not None:
+                        pees[m] = t
+                if pees[m].poll() is None:
+                    running = True
+            for m in fio.nmini:
+                if pees[m] is None:
+                    continue
+                out[m] = open(fifo[m], 'w', 0)
+                out[m].write(pees[m].stdout.read(1))
+                pees[m].stdout.flush()
+                out[m].close()
+
         for m in fio.nmini:
             if pees[m] is None:
                 continue
             out[m] = open(fifo[m], 'w', 0)
-            out[m].write(pees[m].stdout.read(1))
+            out[m].write(pees[m].stdout.read())
             pees[m].stdout.flush()
             out[m].close()
 
-    for m in fio.nmini:
-        out[m] = open(fifo[m], 'w', 0)
-        out[m].write(pees[m].stdout.read())
-        pees[m].stdout.flush()
-        out[m].close()
-
-    return xterms
+        return xterms
 
 
 def main():
     if not exexists('xterm'):
         LOGGER.error("The external program xterm required. Not found.")
         return 1
-    try:
-        get_args()
-    except Exception, err_msg:
-        LOGGER.error(err_msg)
-        return 1
 
+    pf = Pformacl()
+    pf.get_args()
     # Inputs list of raw files and project directory
-    fio = pforma_io.FormaIO(infile=RAW_LST, outdir=PROJECT)
-
+    fio = pforma_io.FormaIO(infile=pf.RAW_LST, outdir=pf.PROJECT)
     # Number of families is (default) number of CPU's + 1 or set
-    fio.set_nmini(NFAMILY)
-
+    fio.set_nmini(pf.NFAMILY)
     # M is the number of mini ph5 files per family, otherwise set in fio.read
-    if M:
-        fio.set_M(M)
-
+    if pf.M:
+        fio.set_M(pf.M)
     # Set UTM zone for segd2ph5 if needed
-    if UTM:
-        fio.set_utm(UTM)
-    if TSPF:
-        fio.set_tspf(TSPF)
-
+    if pf.UTM:
+        fio.set_utm(pf.UTM)
+    if pf.TSPF:
+        fio.set_tspf(pf.TSPF)
     # Don't merge just process raw to PH5
-    if not MERGE:
+    if not pf.MERGE:
         try:
             # Open list of input files
             fio.open()
@@ -185,7 +177,7 @@ def main():
             LOGGER.info("Total raw size: {0:7.2f}GB"
                         .format(float(fio.total_raw / 1024. / 1024. / 1024.)))
 
-            adjust(fio)
+            pf.adjust(fio)
 
             # Set up the processing directory structure, and reset M if mini
             # files already exist
@@ -200,10 +192,8 @@ def main():
         # Resolve JSON db with list of files we are loading (have they been
         # loaded already)
         fio.resolveDB()
-
         # Do the conversions to PH5
-        xterms = run(fio)
-
+        xterms = pf.run(fio)
         # Merge loaded raw files with existing JSON db and write new JSON db
         fio.merge(fio.resolved.keys())
 
