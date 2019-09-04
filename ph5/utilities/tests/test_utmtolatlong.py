699ee8ee2d91ded28649724d7e6166037e9ff189
dsentinel <lloydcarothers@gmail.com>
2019-09-04 13:31:24 -0600

    ready to merge

--- ph5/utilities/tests/test_utmtolatlong.py
+++ ph5/utilities/tests/test_utmtolatlong.py
@@ -8,12 +8,12 @@ class TestUTMconversion(unittest.TestCase):
     def test_is_valid_utm_conversion(self):
         u = UTMConversions()  # PIC Socorro
         self.assertAlmostEqual(u.lat_long(322916, 3771967, 13, 'N'),
-                         (34.07349577107704, -106.91909595147378))
+                               (34.07349577107704, -106.91909595147378))
 
     def test_is_valid_utm_conversion_south(self):
         u = UTMConversions()  # Castle Rock Antarctica
         self.assertAlmostEqual(u.lat_long(540947, 1361594, 58, 'S'),
-                         (-77.81567398301094, 166.73816638798527))
+                               (-77.81567398301094, 166.73816638798527))
 
     def test_for_correct_type(self):
         with self.assertRaises(ValueError):
