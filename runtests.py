26b350a52429e86f819390404781a8cae18c9c2e
dsentinel <lloydcarothers@gmail.com>
2019-09-11 16:37:37 -0600

    Change default verbosity of tests.

--- runtests.py
+++ runtests.py
@@ -8,6 +8,6 @@ from sys import exit
 
 if __name__ == '__main__':
     test_suite = unittest.defaultTestLoader.discover('.')
-    result = unittest.TextTestRunner(verbosity=1).run(test_suite)
+    result = unittest.TextTestRunner(verbosity=3).run(test_suite)
     exit(0 if result.wasSuccessful() else 1)
 
