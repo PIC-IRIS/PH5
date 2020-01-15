1c17cce9825b17d2150f1be04668ab5056a9817c
Lan Dam <ldam@passcal.nmt.edu>
2020-01-15 12:34:14 -0700

    unittest for change in ph5tostationxml.py.PH5toStationXMLParser.get_network_date

---
+++ ph5/clients/tests/test_ph5tostationxml.py
@@ -0,0 +1,138 @@
+'''
+Tests for pforma_io
+'''
+import unittest
+import os
+import sys
+import shutil
+import tempfile
+from mock import patch
+from ph5.core.tests import base_test
+from ph5.clients import ph5tostationxml
+from ph5.utilities import kef2ph5
+
+
+class TestPH5toStationXMLParser(unittest.TestCase):
+    def getParser(self, level, minlat=None, maxlat=None, minlon=None,
+                  maxlon=None, lat=None, lon=None, minrad=None, maxrad=None):
+        self.ph5sxml = [ph5tostationxml.PH5toStationXMLRequest(
+            minlatitude=minlat,
+            maxlatitude=maxlat,
+            minlongitude=minlon,
+            maxlongitude=maxlon,
+            latitude=lat,
+            longitude=lon,
+            minradius=minrad,
+            maxradius=maxrad
+        )]
+        self.mng = ph5tostationxml.PH5toStationXMLRequestManager(
+            sta_xml_obj_list=self.ph5sxml,
+            ph5path=".",
+            nickname="master.ph5",
+            level=level,
+            format="TEXT"
+        )
+        return ph5tostationxml.PH5toStationXMLParser(self.mng)
+
+    def createPH5(self, path, arraykef):
+        kef2ph5.EX = ex = base_test.initialize_ph5("master.ph5", path, True)
+        kef2ph5.PH5 = path + "/master.ph5"
+        kef2ph5.TRACE = False
+
+        kef2ph5.KEFFILE = self.home + arraykef
+
+        kef2ph5.populateTables()
+        # add experiment_t
+        kef2ph5.KEFFILE = self.home + \
+            "/ph5/test_data/metadata/experiment.kef"
+        kef2ph5.populateTables()
+        ex.ph5close()
+
+    def setUp(self):
+        base_test.log_capture_string()
+        # create tmpdir
+        self.home = os.getcwd()
+        self.tmpdir = tempfile.mkdtemp() + "/"
+
+        # create ph5
+        os.chdir(self.tmpdir)
+
+    def tearDown(self):
+        if self._resultForDoCleanups.wasSuccessful():
+            try:
+                shutil.rmtree(self.tmpdir)
+            except Exception as e:
+                print("Cannot remove %s due to the error:%s" %
+                      (self.tmpdir, str(e)))
+        else:
+            errmsg = "%s has FAILED. Inspect files created in %s." \
+                % (self._testMethodName, self.tmpdir)
+            print(errmsg)
+        try:
+            self.mng.ph5.close()
+        except Exception:
+            pass
+        os.chdir(self.home)
+
+    def test_get_network_date(self):
+        """
+        test get_network_date
+        """
+        # add array_t from array_multideploy to test error of getting only the
+        # first deploy time when calculating nettime
+        self.createPH5(self.tmpdir,
+                       "/ph5/test_data/metadata/array_multi_deploy.kef")
+        parser = self.getParser("NETWORK")
+        parser.add_ph5_stationids()
+        parser.read_stations()
+        ret = parser.get_network_date()
+        self.assertTupleEqual(ret, (1561831713.0, 1569680979.0))
+
+    def test_create_obs_network(self):
+        """
+        test create_obs_network
+        """
+        # add array_t from array_multideploy to test error of getting only the
+        # first deploy time when calculating nettime
+        self.createPH5(self.tmpdir,
+                       "/ph5/test_data/metadata/array_multi_deploy.kef")
+        parser = self.getParser("NETWORK")
+        parser.manager.ph5.read_experiment_t()
+        parser.experiment_t = parser.manager.ph5.Experiment_t['rows']
+        parser.add_ph5_stationids()
+        ret = parser.create_obs_network()
+        self.assertEqual(ret.start_date.isoformat(), '2019-06-29T18:08:33')
+        self.assertEqual(ret.end_date.isoformat(), '2019-09-28T14:29:39')
+        self.assertEqual(ret.code, 'AA')
+        self.assertEqual(ret.description, 'PH5 TEST SET')
+
+    def test_read_networks(self):
+        # add array_t from array_multideploy to test error of getting only the
+        # first deploy time when calculating nettime
+        self.createPH5(self.tmpdir,
+                       "/ph5/test_data/metadata/array_multi_deploy.kef")
+        parser = self.getParser("NETWORK")
+        ret = parser.read_networks('.')
+        self.assertEqual(ret.start_date.isoformat(), '2019-06-29T18:08:33')
+        self.assertEqual(ret.end_date.isoformat(), '2019-09-28T14:29:39')
+        self.assertEqual(ret.code, 'AA')
+        self.assertEqual(ret.description, 'PH5 TEST SET')
+
+    def test_main(self):
+        # add array_t from array_multideploy to test error of getting only the
+        # first deploy time when calculating nettime
+        self.createPH5(self.tmpdir,
+                       "/ph5/test_data/metadata/array_multi_deploy.kef")
+        testargs = ['ph5tostationxml', '-n', 'master',
+                    '--level', 'network', '-f', 'text']
+        with patch.object(sys, 'argv', testargs):
+            with base_test.captured_output() as (out, err):
+                ph5tostationxml.main()
+        output = out.getvalue().strip().split("\n")
+        self.assertEqual(
+            output[1],
+            "AA|PH5 TEST SET|2019-06-29T18:08:33|2019-09-28T14:29:39|1")
+
+
+if __name__ == "__main__":
+    unittest.main()
