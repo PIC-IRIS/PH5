dbad160089733042c4cf004d791711380eb2c3ee
Lan Dam <ldam@passcal.nmt.edu>
2020-01-15 12:32:05 -0700

    add captured_output() to capture stdout into ph5/core/tests/base_test.py

--- ph5/core/tests/base_test.py
+++ ph5/core/tests/base_test.py
@@ -3,16 +3,36 @@ General methods for testing
 """
 
 import logging
+import sys
 from StringIO import StringIO
+from contextlib import contextmanager
 from ph5 import logger, ch as CH
 from ph5.core import experiment
+
+
+@contextmanager
+def captured_output():
+    """
+    to capture stdout:
+    from ph5.core.tests import base_test
+    with base_test.captured_output() as (out, err):
+        //some function
+    output = out.getvalue().strip()
+    """
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        yield sys.stdout, sys.stderr
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
 
 
 def log_capture_string():
     """
     to capture log:
-    from ph5.core.tests import log_capture_string
-    capture = log_capture_string()
+    from ph5.core.tests import base_test
+    capture = base_test.log_capture_string()
     loglines = capture.getvalue().split("\n")
     """
     capture = StringIO()
