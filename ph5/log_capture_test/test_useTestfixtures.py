e5df2447738e4c2e358f96ca9100f7ef32d25d6b
Lan Dam <ldam@passcal.nmt.edu>
2020-02-07 12:10:14 -0700

    use testfitures to test log in more detail

--- ph5/log_capture_test/test_useTestfixtures.py
+++ ph5/log_capture_test/test_useTestfixtures.py
@@ -2,6 +2,7 @@ import unittest
 from testfixtures import LogCapture
 from ph5.log_capture_test import logEx
 import ph5
+import logging
 
 
 class TestLogEx(unittest.TestCase):
@@ -18,17 +19,32 @@ class TestLogEx(unittest.TestCase):
         ph5.logger.addHandler(ph5.ch)
 
     def test_makeLog(self):
-        path = 'ph5'
+        logger_name = 'ph5'
 
+        # check the whole log
         with LogCapture() as log:
             logEx.makeLog()
         log.check(
-            (path, 'WARNING', 'My first warning'),
-            (path, 'INFO', 'My first info'),
-            (path, 'ERROR', 'My first error'),
-            (path, 'ERROR', 'My second error'),
-            (path, 'WARNING', 'My second warning'),
-            (path, 'INFO', 'My second info'))
+            (logger_name, 'WARNING', 'My first warning'),
+            (logger_name, 'INFO', 'My first info'),
+            (logger_name, 'ERROR', 'My first error'),
+            (logger_name, 'ERROR', 'My second error'),
+            (logger_name, 'WARNING', 'My second warning'),
+            (logger_name, 'INFO', 'My second info'))
+
+        # check if a log message exists
+        log.check_present((logger_name, 'WARNING', 'My second warning'))
+
+        # check ERROR only
+        with LogCapture() as log:
+            log.setLevel(logging.ERROR)
+            logEx.makeLog()
+        log.check(
+            (logger_name, 'ERROR', 'My first error'),
+            (logger_name, 'ERROR', 'My second error'))
+
+        # check specific errors
+        self.assertEqual(log.records[1].msg, 'My second error')
 
 
 if __name__ == "__main__":
