4b309e3530302df7482d5e17098729b6f6868e05
Lan Dam <ldam@passcal.nmt.edu>
2020-02-14 10:21:41 -0700

    remove logger's handler to prevent log from being printed out to screen

--- runtests.py
+++ runtests.py
@@ -5,9 +5,15 @@ Script to run all tests in the entire project
 
 import unittest
 from sys import exit
+import ph5
+
 
 if __name__ == '__main__':
+    # enable propagating to higher loggers
+    ph5.logger.propagate = 1
+    # disable writing log to console
+    ph5.logger.removeHandler(ph5.ch)
+
     test_suite = unittest.defaultTestLoader.discover('.')
     result = unittest.TextTestRunner(verbosity=3).run(test_suite)
     exit(0 if result.wasSuccessful() else 1)
-
