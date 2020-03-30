fb991f2e6fca4db5513511c8090b988d4e09d14a
Lan Dam <ldam@passcal.nmt.edu>
2020-03-30 16:06:40 -0600

    Revert "don't need to remove file in test because TempDirTestCase will cleanup all temp dir used for tests"
    This reverts commit 4dc0b7dc38f889d54d6c29171b018039f2f23dcc.
    
    revert to merge #388

--- ph5/clients/tests/test_ph5availability.py
+++ ph5/clients/tests/test_ph5availability.py
@@ -1733,6 +1733,7 @@ class TestPH5Availability(TempDirTestCase, LogTestCase):
         with open('test', 'r') as content_file:
             content = content_file.read().strip()
         self.assertEqual(content, "this is a text line")
+        os.remove('test')
 
     def test_get_geoCSV_report(self):
         """
