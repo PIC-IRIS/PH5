8af29ea15fac28373a4c604b3a80afc490e23e22
Lan Dam <ldam@passcal.nmt.edu>
2020-04-15 10:15:16 -0600

    modify class TestPH5toStationXMLParser_latlon to use new test_base, getParser; add test_create_obs_network to check log info; self.errmsgs keeps all errors for convinience to test

--- ph5/clients/tests/test_ph5tostationxml.py
+++ ph5/clients/tests/test_ph5tostationxml.py
@@ -6,14 +6,11 @@ import os
 import sys
 import logging
 from mock import patch
-import shutil
-import tempfile
 
 from testfixtures import OutputCapture, LogCapture
 
 from ph5.utilities import kef2ph5
 from ph5.clients import ph5tostationxml
-from ph5.core import experiment
 from ph5.core.tests.test_base import LogTestCase, TempDirTestCase,\
     initialize_ex
 
@@ -24,7 +21,6 @@ def kef_to_ph5(ph5path, nickname, kefpath, keflist, ex=None):
     (The task of deleting table before adding the table should happen before
     calling this function. If it is required to have a delete function for all,
     it should be written in nuke_table.py)
-
     :para ph5path: path to ph5 file
     :type ph5path: string
     :para kefpath: path to kef files
@@ -54,6 +50,29 @@ def kef_to_ph5(ph5path, nickname, kefpath, keflist, ex=None):
         kef2ph5.EX.ph5close()
 
 
+def getParser(level, minlat=None, maxlat=None, minlon=None,
+              maxlon=None, lat=None, lon=None, minrad=None, maxrad=None):
+    ph5sxml = [ph5tostationxml.PH5toStationXMLRequest(
+        minlatitude=minlat,
+        maxlatitude=maxlat,
+        minlongitude=minlon,
+        maxlongitude=maxlon,
+        latitude=lat,
+        longitude=lon,
+        minradius=minrad,
+        maxradius=maxrad
+    )]
+    mng = ph5tostationxml.PH5toStationXMLRequestManager(
+        sta_xml_obj_list=ph5sxml,
+        ph5path=".",
+        nickname="master.ph5",
+        level=level,
+        format="TEXT"
+    )
+    parser = ph5tostationxml.PH5toStationXMLParser(mng)
+    return ph5sxml, mng, parser
+
+
 class TestPH5toStationXMLParser_main(LogTestCase, TempDirTestCase):
     def test_main(self):
         # array_multideploy: same station different deploy times
@@ -81,32 +100,11 @@ class TestPH5toStationXMLParser(LogTestCase, TempDirTestCase):
                    'master.ph5',
                    os.path.join(self.home, "ph5/test_data/metadata"),
                    ["array_multi_deploy.kef", "experiment.kef"])
-        self.parser = self.getParser("NETWORK")
+        self.ph5sxml, self.mng, self.parser = getParser("NETWORK")
 
     def tearDown(self):
         self.mng.ph5.close()
         super(TestPH5toStationXMLParser, self).tearDown()
-
-    def getParser(self, level, minlat=None, maxlat=None, minlon=None,
-                  maxlon=None, lat=None, lon=None, minrad=None, maxrad=None):
-        self.ph5sxml = [ph5tostationxml.PH5toStationXMLRequest(
-            minlatitude=minlat,
-            maxlatitude=maxlat,
-            minlongitude=minlon,
-            maxlongitude=maxlon,
-            latitude=lat,
-            longitude=lon,
-            minradius=minrad,
-            maxradius=maxrad
-        )]
-        self.mng = ph5tostationxml.PH5toStationXMLRequestManager(
-            sta_xml_obj_list=self.ph5sxml,
-            ph5path=".",
-            nickname="master.ph5",
-            level=level,
-            format="TEXT"
-        )
-        return ph5tostationxml.PH5toStationXMLParser(self.mng)
 
     def test_get_network_date(self):
         self.parser.add_ph5_stationids()
@@ -134,71 +132,40 @@ class TestPH5toStationXMLParser(LogTestCase, TempDirTestCase):
         self.assertEqual(ret.description, 'PH5 TEST SET')
 
 
-class TestPH5toStationXMLParser_latlon(unittest.TestCase):
+class TestPH5toStationXMLParser_latlon(LogTestCase, TempDirTestCase):
     def setUp(self):
-        # create tmpdir
-        self.home = os.getcwd()
-        self.tmpdir = tempfile.mkdtemp() + "/"
+        super(TestPH5toStationXMLParser_latlon, self).setUp()
 
-        # create ph5
-        os.chdir(self.tmpdir)
-        kef2ph5.EX = ex = experiment.ExperimentGroup(nickname="master.ph5")
-        ex.ph5open(True)
-        ex.initgroup()
-        kef2ph5.PH5 = self.tmpdir + "/master.ph5"
-        kef2ph5.TRACE = False
+        kef_to_ph5(self.tmpdir,
+                   'master.ph5',
+                   os.path.join(self.home, "ph5/test_data/metadata"),
+                   ["array_ph5tostationxml.kef", "experiment.kef"])
 
-        # add array_t from array_ph5tostationxml with errors for:
-        # 1: lat, lon based on param set for PH5toStationXMLRequest
-        kef2ph5.KEFFILE = self.home + \
-            "/ph5/test_data/metadata/array_ph5tostationxml.kef"
-        kef2ph5.populateTables()
-        # add experiment_t
-        kef2ph5.KEFFILE = self.home + \
-            "/ph5/test_data/metadata/experiment.kef"
-        kef2ph5.populateTables()
+        self.ph5sxml, self.mng, self.parser = getParser(
+            "NETWORK", 34, 40, -111, -105, 36, -107, 0, 3)
 
-        ex.ph5close()
-        self.ph5sxml = [ph5tostationxml.PH5toStationXMLRequest(
-            minlatitude=34.,
-            maxlatitude=40.,
-            minlongitude=-111,
-            maxlongitude=-105.,
-            latitude=36,
-            longitude=-107,
-            minradius=0,
-            maxradius=3
-        )]
-        self.mng = ph5tostationxml.PH5toStationXMLRequestManager(
-            sta_xml_obj_list=self.ph5sxml,
-            ph5path=".",
-            nickname="master.ph5",
-            level="NETWORK",
-            format="TEXT"
-        )
-        self.parser = ph5tostationxml.PH5toStationXMLParser(self.mng)
+        # errors in array_latlon_err.kef
+        self.errmsgs = [
+            "array 1111, station 001,"
+            " channel 1: Lat -107.0 not in range [-90,90]",
+            "array 1112, station 001, channel 1:"
+            " Lon 182.0 not in range [-180,180]",
+            "array 1113, station 001, channel 1: Box intersection: Lat 70.0"
+            " not in range [34,40] or Lon 100.0 not in range [-111,-105]",
+            "array 1114, station 001, channel 1: Box intersection: Lat 35.0"
+            " not in range [34,40] or Lon 100.0 not in range [-111,-105]",
+            "array 1116, station 001, channel 1: lat,lon=35.0,-111.0:"
+            " radial intersection between a point radius boundary [0, 3]"
+            " and a lat/lon point [36, -107]",
+            "array 1117, station 001, channel 1: lat,lon=40.0,-106.0:"
+            " radial intersection between a point radius boundary [0, 3]"
+            " and a lat/lon point [36, -107]"]
 
     def tearDown(self):
-        if self._resultForDoCleanups.wasSuccessful():
-            try:
-                shutil.rmtree(self.tmpdir)
-            except Exception as e:
-                print("Cannot remove %s due to the error:%s" %
-                      (self.tmpdir, str(e)))
-        else:
-            errmsg = "%s has FAILED. Inspect files created in %s." \
-                % (self._testMethodName, self.tmpdir)
-            print(errmsg)
-        try:
-            self.mng.ph5.close()
-        except Exception:
-            pass
-        os.chdir(self.home)
+        self.mng.ph5.close()
+        super(TestPH5toStationXMLParser_latlon, self).tearDown()
 
     def test_is_lat_lon_match(self):
-        """
-        test is_lat_lon_match
-        """
         # 1. latitude not in (-90, 90)
         ret = self.parser.is_lat_lon_match(self.ph5sxml[0], -107, 100)
         self.assertEqual(ret, 'Lat -107 not in range [-90,90]')
@@ -210,14 +177,14 @@ class TestPH5toStationXMLParser_latlon(unittest.TestCase):
         # 3. latitude not in minlatitude=34 and maxlatitude=40
         ret = self.parser.is_lat_lon_match(self.ph5sxml[0], 70, 100)
         self.assertEqual(
-            ret, "Box intersection: Lat 70 not in range [34.0,40.0] "
-            "or Lon 100 not in range [-111,-105.0]")
+            ret, "Box intersection: Lat 70 not in range [34,40] "
+            "or Lon 100 not in range [-111,-105]")
 
         # 4. longitude not in minlongitude=-111 and maxlongitude=-105
         ret = self.parser.is_lat_lon_match(self.ph5sxml[0], 35, 100)
         self.assertEqual(
-            ret, "Box intersection: Lat 35 not in range [34.0,40.0] "
-            "or Lon 100 not in range [-111,-105.0]")
+            ret, "Box intersection: Lat 35 not in range [34,40] "
+            "or Lon 100 not in range [-111,-105]")
 
         # 5. pass all checks
         ret = self.parser.is_lat_lon_match(self.ph5sxml[0], 35, -106)
@@ -236,43 +203,24 @@ class TestPH5toStationXMLParser_latlon(unittest.TestCase):
             "radius boundary [0, 3] and a lat/lon point [36, -107]")
 
     def test_read_station(self):
-        """
-        test read station method
-        """
+        self.parser.add_ph5_stationids()
+        self.parser.read_stations()
+        self.assertEqual(self.parser.unique_errmsg, self.errmsgs)
+
+    def test_create_obs_network(self):
+        self.parser.manager.ph5.read_experiment_t()
+        self.parser.experiment_t = self.parser.manager.ph5.Experiment_t['rows']
         self.parser.add_ph5_stationids()
         with LogCapture() as log:
-            log.setLevel(logging.WARNING)
-            self.parser.read_stations()
+            log.setLevel(logging.ERROR)
+            self.parser.create_obs_network()
 
-        self.assertEqual(
-            log.records[0].msg,
-            "station 1111, array 001: Lat -107.0 not in range [-90,90]")
-
-        self.assertEqual(
-            log.records[1].msg,
-            "station 1112, array 001: Lon 182.0 not in range [-180,180]")
-
-        self.assertEqual(
-            log.records[2].msg,
-            "station 1113, array 001: Box intersection: Lat 70.0 "
-            "not in range [34.0,40.0] or Lon 100.0 not in range [-111,-105.0]")
-
-        self.assertEqual(
-            log.records[3].msg,
-            "station 1114, array 001: Box intersection: Lat 35.0 "
-            "not in range [34.0,40.0] or Lon 100.0 not in range [-111,-105.0]")
-
-        self.assertEqual(
-            log.records[4].msg,
-            "station 1116, array 001: lat,lon=35.0,-111.0: "
-            "radial intersection between a point radius boundary [0, 3] and "
-            "a lat/lon point [36, -107]")
-
-        self.assertEqual(
-            log.records[5].msg,
-            "station 1117, array 001: lat,lon=40.0,-106.0: "
-            "radial intersection between a point radius boundary [0, 3] and "
-            "a lat/lon point [36, -107]")
+            self.assertEqual(log.records[0].msg, self.errmsgs[0])
+            self.assertEqual(log.records[1].msg, self.errmsgs[1])
+            self.assertEqual(log.records[2].msg, self.errmsgs[2])
+            self.assertEqual(log.records[3].msg, self.errmsgs[3])
+            self.assertEqual(log.records[4].msg, self.errmsgs[4])
+            self.assertEqual(log.records[5].msg, self.errmsgs[5])
 
 
 if __name__ == "__main__":
