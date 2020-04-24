824624096d163660515157857f4de7128e198bb1
damhuonglan <30272624+damhuonglan@users.noreply.github.com>
2020-04-23 18:25:49 -0600

    Fix #360: pforma duplicate index_t/array_t from A to Sigma (#361)
    * #360 skip loading array_t/index_t from A/ to Sigma/ because master.ph5 has been copied from A/ to Sigma/ first
    
    * In segd2ph5.py move main process into process() so that it can be called from testing
    
    * test pforma_io.FormaIO.unite() for the change to fix issue #360
    
    * replace logger with LOGGER
    
    * remove logger's handler to prevent log from being printed out to screen
    
    * replace using function captured_output() with testfixtures.OutputCapture
    
    * add testfixtures to the list of packages needed to be installed
    
    * add StringIO handler to prevent message 'No handlers could be found
    
    * resolve confict when merge master
    
    * apply LogTestCase, TempDirTestCase
    
    * run segd2ph5.main() to avoid too much code; reorder for readable; use initgroup so that don't have to close and re-initializeph5 for tabletokef
    
    * Revert "run segd2ph5.main() to avoid too much code; reorder for readable; use initgroup so that don't have to close and re-initializeph5 for tabletokef"
    
    This reverts commit 1e5548dc3aa60b8915455f22d71eb4b127e0766f.
    
    * Revert "apply LogTestCase, TempDirTestCase"
    
    This reverts commit 135e41d335b3a4325b270ca22f48ab3bf5accc9b.
    
    * Revert "resolve confict when merge master"
    
    This reverts commit 3260cf94d6674f011a2e2d8cf857d5bd93359b7f.
    
    * reset files to master's: segd2ph5, runtests
    
    * run segd2ph5.main() to avoid too much code; reorder for readable; use initgroup so that don't have to close and re-initialize_ph5 for tabletokef
    
    * reorder import, use assertEqual instead of assertDictEqual

---
+++ ph5/utilities/tests/test_pforma_io.py
@@ -0,0 +1,69 @@
+'''
+Tests for pforma_io
+'''
+import unittest
+import os
+import sys
+
+from mock import patch
+from testfixtures import OutputCapture
+
+from ph5.utilities import segd2ph5, pforma_io, tabletokef
+from ph5.core.tests.test_base import LogTestCase, TempDirTestCase
+
+
+class TestPforma(LogTestCase, TempDirTestCase):
+
+    def test_unite(self):
+        """
+        creating element folders and A's master.ph5 to imitate the "Run"
+        in pforma
+        """
+        # create element folders
+        MINIS = ['A', 'B', 'C', 'D']
+        for n in MINIS:
+            os.mkdir(os.path.join(self.tmpdir, n))
+
+        # add data to A/master.ph5
+        os.chdir(os.path.join(self.tmpdir, "A"))
+        testargs = ['segdtoph5', '-n', 'master.ph5', '-r',
+                    os.path.join(self.home, "ph5/test_data/segd/3ch.fcnt")]
+        with patch.object(sys, 'argv', testargs):
+            segd2ph5.main()
+
+        # run unite that create ph5 files in Sigma/
+        fio = pforma_io.FormaIO(infile="", outdir=self.tmpdir)
+        # fio.initialize_ph5 creates ph5 file and doesn't leave it open
+        # so doesn't need to close
+        fio.initialize_ph5()
+        with OutputCapture():
+            fio.unite("Sigma")
+
+        # get array_t and index_t in A/
+        tabletokef.PH5 = "master.ph5"
+        tabletokef.PATH = os.path.join(self.tmpdir, "A")
+        tabletokef.initialize_ph5()  # need to close
+        ARRAY_T_A = tabletokef.readPH5(tabletokef.EX, "", "", "All_Array_t")
+        INDEX_T_A = tabletokef.readPH5(tabletokef.EX, "", "", "Index_t")
+
+        # get array_t and index_t in Sigma/
+        self.path = os.path.join(self.tmpdir, "Sigma")
+        tabletokef.EX.initgroup()
+        ARRAY_T_Sigma = tabletokef.readPH5(
+            tabletokef.EX, "", "", "All_Array_t")
+        INDEX_T_Sigma = tabletokef.readPH5(tabletokef.EX, "", "", "Index_t")
+        tabletokef.EX.ph5close()
+
+        # check array_t and index_t in Sigma/ are the same as in A/
+        # (not duplicated)
+        self.assertEqual(ARRAY_T_A.keys(), ARRAY_T_Sigma.keys())
+        array_key = ARRAY_T_A.keys()[0]
+        self.assertEqual(ARRAY_T_A[array_key].rows[0],
+                         ARRAY_T_Sigma[array_key].rows[0])
+
+        self.assertEqual(INDEX_T_A.rows[0],
+                         INDEX_T_Sigma.rows[0])
+
+
+if __name__ == "__main__":
+    unittest.main()
