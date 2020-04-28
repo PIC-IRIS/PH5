971ac848d5c45278656f2d339301e1c5928effef
Lan <ldam@passcal.nmt.edu>
2020-04-28 14:36:19 -0600

    add close EXREC to prevent minifile still open when existing

--- ph5/utilities/tests/test_ph5validate.py
+++ ph5/utilities/tests/test_ph5validate.py
@@ -4,6 +4,7 @@ Tests for ph5validate
 import unittest
 import os
 import sys
+import logging
 
 from mock import patch
 from testfixtures import OutputCapture
@@ -13,28 +14,97 @@ from ph5.core import ph5api
 from ph5.core.tests.test_base import LogTestCase, TempDirTestCase
 
 
+def create_ph5_data(home):
+    # create master.ph5
+    testargs = ['initialize_ph5', '-n', 'master.ph5']
+    with patch.object(sys, 'argv', testargs):
+        initialize_ph5.main()
+    # add data
+    testargs = ['125atoph5', '-n', 'master.ph5', '-r',
+                os.path.join(home, "ph5/test_data/rt125a/I2183RAW.TRD")]
+    with patch.object(sys, 'argv', testargs):
+        with OutputCapture():
+            texan2ph5.main()
+    # add metadata
+    testargs = ['keftoph5', '-n', 'master.ph5', '-k',
+                os.path.join(home,
+                             "ph5/test_data/metadata/array_t_9_validate.kef")]
+    with patch.object(sys, 'argv', testargs):
+        kef2ph5.main()
+
+
+class TestPh5Validate_main(TempDirTestCase, LogTestCase):
+    def setUp(self):
+        super(TestPh5Validate_main, self).setUp()
+        create_ph5_data(self.home)
+
+    def test_main(self):
+        testargs = ['ph5_validate', '-n', 'master.ph5', '-p', self.tmpdir,
+                    '-l', 'WARNING']
+        with patch.object(sys, 'argv', testargs):
+            with OutputCapture():
+                ph5validate.main()
+        with open('ph5_validate.log') as f:
+            all_logs = f.read().split("-=-=-=-=-=-=-=-=-\n")
+
+        self.assertEqual(
+            all_logs[2],
+            'ERROR: Experiment_t does not exist. '
+            'run experiment_t_gen to create table\n')
+        self.assertEqual(
+            all_logs[3],
+            'Station 9001 Channel 1\n1 error, 2 warning, 0 info\n')
+        self.assertEqual(
+            all_logs[4],
+            'ERROR: No Response table found. Have you run resp_load yet?\n'
+            'WARNING: No station description found.\n'
+            'WARNING: Data exists before deploy time: 7 seconds.\n')
+        self.assertEqual(
+            all_logs[5],
+            'Station 9002 Channel 1\n1 error, 2 warning, 0 info\n')
+        self.assertEqual(
+            all_logs[6],
+            'ERROR: No Response table found. Have you run resp_load yet?\n'
+            'WARNING: No station description found.\n'
+            'WARNING: Data exists after pickup time: 36 seconds.\n')
+        self.assertEqual(
+            all_logs[7],
+            'Station 9003 Channel 1\n1 error, 2 warning, 0 info\n')
+        self.assertEqual(
+            all_logs[8],
+            'ERROR: No Response table found. Have you run resp_load yet?\n'
+            'WARNING: No station description found.\n'
+            'WARNING: Data exists after pickup time: 2 seconds.\n')
+
+    def test_get_args(self):
+        # wrong path
+        testargs = ['ph5_validate', '-n', 'master1.ph5']
+        with patch.object(sys, 'argv', testargs):
+            self.assertRaises(ValueError, ph5validate.get_args)
+        # wrong level
+        testargs = ['ph5_validate', '-n', 'master.ph5', '-l', 'EROR']
+        with patch.object(sys, 'argv', testargs):
+            self.assertRaises(ValueError, ph5validate.get_args)
+        # check values return and LOGGER's params
+        testargs = ['ph5_validate', '-n', 'master.ph5', '-p', self.tmpdir,
+                    '-l', 'WARNING']
+        with patch.object(sys, 'argv', testargs):
+            ret = ph5validate.get_args()
+        self.assertEqual(ret.level, 'WARNING')
+        self.assertEqual(ret.nickname, 'master.ph5')
+        self.assertEqual(ret.outfile, 'ph5_validate.log')
+        self.assertEqual(ret.ph5path, self.tmpdir)
+        self.assertEqual(ret.verbose, False)
+        self.assertIsInstance(ph5validate.LOGGER.handlers[0],
+                              logging.FileHandler)
+        self.assertEqual(ph5validate.LOGGER.handlers[0].baseFilename,
+                         os.path.join(self.tmpdir, 'ph5_validate.log'))
+
+
 class TestPh5Validate(TempDirTestCase, LogTestCase):
     def setUp(self):
         super(TestPh5Validate, self).setUp()
-        # create master.ph5
-        testargs = ['initialize_ph5', '-n', 'master.ph5']
-        with patch.object(sys, 'argv', testargs):
-            initialize_ph5.main()
-        # add data
-        testargs = ['texan2ph5', '-n', 'master.ph5', '-r',
-                    os.path.join(self.home,
-                                 "ph5/test_data/rt125a/I2183RAW.TRD")]
-        with patch.object(sys, 'argv', testargs):
-            with OutputCapture():
-                texan2ph5.main()
-        # add metadata
-        testargs = ['kef2ph5', '-n', 'master.ph5', '-k',
-                    os.path.join(
-                        self.home,
-                        "ph5/test_data/metadata/array_t_9_validate.kef")]
-        with patch.object(sys, 'argv', testargs):
-            kef2ph5.main()
-
+        create_ph5_data(self.home)
         self.ph5_object = ph5api.PH5(
             nickname='master.ph5', path=self.tmpdir)
         self.ph5validate = ph5validate.PH5Validate(
@@ -71,10 +141,6 @@ class TestPh5Validate(TempDirTestCase, LogTestCase):
                          [1550849950,
                           'Data exists before deploy time: 7 seconds.'])
 
-        self.assertEqual(Dtime['max_pickup_time'],
-                         [1550850187,
-                          'Data exists after pickup time: 2 seconds.'])
-
     def test_check_station_completeness(self):
         self.ph5validate.das_time = {
             ('12183', 1, 500):
@@ -83,8 +149,6 @@ class TestPh5Validate(TempDirTestCase, LogTestCase):
                               (1550850125, 1550850187, '9003')],
              'min_deploy_time': [1550849950,
                                  'Data exists before deploy time: 7 seconds.'],
-             'max_pickup_time': [1550850187,
-                                 'Data exists after deploy time: 2 seconds.']
              }
         }
 
@@ -97,12 +161,6 @@ class TestPh5Validate(TempDirTestCase, LogTestCase):
         ret = self.ph5validate.check_station_completeness(station)
         warnings = ret[1]
         self.assertIn('Data exists before deploy time: 7 seconds.', warnings)
-
-        # check warning after max_pickup_time
-        station = arraybyid.get('9003')[1][0]
-        ret = self.ph5validate.check_station_completeness(station)
-        warnings = ret[1]
-        self.assertIn('Data exists after deploy time: 2 seconds.', warnings)
 
         # check warning data after pickup time
         station = arraybyid.get('9002')[1][0]
