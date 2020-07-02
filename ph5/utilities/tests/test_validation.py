9c887c4a2280d7f00cf7483f51251e4d4785af40
Lan <ldam@passcal.nmt.edu>
2020-07-02 14:55:32 -0600

    separate warnings, errors in validation and unittest

--- ph5/utilities/tests/test_validation.py
+++ ph5/utilities/tests/test_validation.py
@@ -1,5 +1,5 @@
 '''
-Tests for ph5validate
+Tests for validation
 '''
 import unittest
 
@@ -14,8 +14,10 @@ class TestValidation(unittest.TestCase):
                    'location/Y/units_s': 'degrees',
                    'location/Z/value_d': 1047,
                    'location/Z/units_s': 'm'}
+        errors = []
         warnings = []
-        validation.check_lat_lon_elev(station, warnings)
+        validation.check_lat_lon_elev(station, errors, warnings)
+        self.assertEqual(errors, [])
         self.assertEqual(warnings, [])
 
         station = {'location/X/value_d': 190.0,
@@ -24,15 +26,18 @@ class TestValidation(unittest.TestCase):
                    'location/Y/units_s': '',
                    'location/Z/value_d': '',
                    'location/Z/units_s': ''}
-        validation.check_lat_lon_elev(station, warnings)
-        self.assertEqual(warnings,
-                         ['Channel longitude 190.0 not in range [-180,180]',
-                          'No Station location/X/units_s value found.',
-                          'Channel latitude -100.0 not in range [-90,90]',
-                          'No Station location/Y/units_s value found.',
-                          'No Channel location/Z/value_d value found.',
-                          'No Station location/Z/units_s value found.'])
-
+        validation.check_lat_lon_elev(station, errors, warnings)
+        self.assertEqual(
+            errors,
+            ['Channel longitude 190.0 not in range [-180,180]',
+             'Channel latitude -100.0 not in range [-90,90]',
+             'No Channel location/Z/value_d value found.'])
+        self.assertEqual(
+            warnings,
+            ['No Station location/X/units_s value found.',
+             'No Station location/Y/units_s value found.',
+             'No Station location/Z/units_s value found.'])
+        errors = []
         warnings = []
         station = {'location/X/value_d': 'ABC',
                    'location/X/units_s': '',
@@ -40,16 +45,19 @@ class TestValidation(unittest.TestCase):
                    'location/Y/units_s': '',
                    'location/Z/value_d': '',
                    'location/Z/units_s': ''}
-        validation.check_lat_lon_elev(station, warnings)
+        validation.check_lat_lon_elev(station, errors, warnings)
+        self.assertEqual(
+            errors,
+            ['Channel longitude ABC is not a number.',
+             'No Channel latitude value found.',
+             'No Channel location/Z/value_d value found.'])
         self.assertEqual(
             warnings,
-            ['Channel longitude ABC is not a number.',
-             'No Station location/X/units_s value found.',
-             'No Channel latitude value found.',
+            ['No Station location/X/units_s value found.',
              'No Station location/Y/units_s value found.',
-             'No Channel location/Z/value_d value found.',
              'No Station location/Z/units_s value found.'])
 
+        errors = []
         warnings = []
         station = {'location/X/value_d': 0,
                    'location/X/units_s': None,
@@ -57,14 +65,16 @@ class TestValidation(unittest.TestCase):
                    'location/Y/units_s': None,
                    'location/Z/value_d': None,
                    'location/Z/units_s': None}
-        validation.check_lat_lon_elev(station, warnings)
+        validation.check_lat_lon_elev(station, errors, warnings)
+        self.assertEqual(
+            errors,
+            ['No Channel latitude value found.',
+             'No Channel location/Z/value_d value found.'])
         self.assertEqual(
             warnings,
             ['Channel longitude seems to be 0. Is this correct???',
              'No Station location/X/units_s value found.',
-             'No Channel latitude value found.',
              'No Station location/Y/units_s value found.',
-             'No Channel location/Z/value_d value found.',
              'No Station location/Z/units_s value found.'])
 
 
--- ph5/utilities/validation.py
+++ ph5/utilities/validation.py
@@ -7,6 +7,8 @@ def check_lat_lon_elev(station, errors, warnings):
     if station['location/X/value_d'] == 0:
         warnings.append("Channel longitude seems to be 0. Is this correct???")
     if station['location/X/value_d'] in [None, '']:
+        # this will not happen
+        # if change in kef will be added to array as 0.0
         errors.append("No Channel longitude value found.")
     else:
         try:
@@ -20,8 +22,6 @@ def check_lat_lon_elev(station, errors, warnings):
                           % station['location/X/value_d'])
 
     if station['location/X/units_s'] in [None, '']:
-        # this will not happen
-        # if change in kef will be added to array as 0.0
         warnings.append("No Station location/X/units_s value found.")
 
     if station['location/Y/value_d'] == 0:
@@ -40,12 +40,12 @@ def check_lat_lon_elev(station, errors, warnings):
                           % station['location/Y/value_d'])
 
     if station['location/Y/units_s'] in [None, '']:
-        # this will not happen
-        # if change in kef will be added to array as 0.0
         warnings.append("No Station location/Y/units_s value found.")
 
     if station['location/Z/value_d'] in [None, '']:
+        # this will not happen
+        # if change in kef will be added to array as 0.0
         errors.append("No Channel location/Z/value_d value found.")
 
     if station['location/Z/units_s'] in [None, '']:
-        errors.append("No Station location/Z/units_s value found.")
+        warnings.append("No Station location/Z/units_s value found.")
