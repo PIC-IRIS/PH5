c220dd7aa7b77f2d2dacb1ca3c1e52f82a2f8227
Lan <ldam@passcal.nmt.edu>
2020-07-02 14:35:44 -0600

    separate warning, errors

--- ph5/utilities/validation.py
+++ ph5/utilities/validation.py
@@ -3,9 +3,9 @@ common functions for validation
 """
 
 
-def check_lat_lon_elev(station, errors):
+def check_lat_lon_elev(station, errors, warnings):
     if station['location/X/value_d'] == 0:
-        errors.append("Channel longitude seems to be 0. Is this correct???")
+        warnings.append("Channel longitude seems to be 0. Is this correct???")
     if station['location/X/value_d'] in [None, '']:
         errors.append("No Channel longitude value found.")
     else:
@@ -14,14 +14,18 @@ def check_lat_lon_elev(station, errors):
                 errors.append("Channel longitude %s not in range [-180,180]"
                               % station['location/X/value_d'])
         except ValueError:
+            # this will not happen
+            # if change in kef will be added to array as 0.0
             errors.append("Channel longitude %s is not a number."
                           % station['location/X/value_d'])
 
     if station['location/X/units_s'] in [None, '']:
-        errors.append("No Station location/X/units_s value found.")
+        # this will not happen
+        # if change in kef will be added to array as 0.0
+        warnings.append("No Station location/X/units_s value found.")
 
     if station['location/Y/value_d'] == 0:
-        errors.append("Channel latitude seems to be 0. Is this correct???")
+        warnings.append("Channel latitude seems to be 0. Is this correct???")
     if station['location/Y/value_d'] in [None, '']:
         errors.append("No Channel latitude value found.")
     else:
@@ -30,11 +34,15 @@ def check_lat_lon_elev(station, errors):
                 errors.append("Channel latitude %s not in range [-90,90]"
                               % station['location/Y/value_d'])
         except ValueError:
+            # this will not happen
+            # if change in kef will be added to array as 0.0
             errors.append("Channel latitude %s is not a number."
                           % station['location/Y/value_d'])
 
     if station['location/Y/units_s'] in [None, '']:
-        errors.append("No Station location/Y/units_s value found.")
+        # this will not happen
+        # if change in kef will be added to array as 0.0
+        warnings.append("No Station location/Y/units_s value found.")
 
     if station['location/Z/value_d'] in [None, '']:
         errors.append("No Channel location/Z/value_d value found.")
