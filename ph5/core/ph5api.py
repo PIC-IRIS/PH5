0e453b2d464ec0ee66d50df36919d1d70872c98e
timronan <tronan@iris.washington.edu>
2020-08-18 13:48:02 -0700

    Remove else block. Remove duplicated data check.

--- ph5/core/ph5api.py
+++ ph5/core/ph5api.py
@@ -1286,8 +1286,7 @@ class PH5(experiment.ExperimentGroup):
         if data is None:
             return [Trace(np.array([]), start_fepoch, 0., 0, sample_rate,
                           None, None, das_t, None, None, clock=clock)]
-        else:
-            calc_stop_fepoch = trace_start_fepoch + (len(data) / sr)
+        calc_stop_fepoch = trace_start_fepoch + (len(data) / sr)
 
         # calculate number of overextending samples
         num_overextend_samples = int(math.floor(calc_stop_fepoch -
@@ -1298,10 +1297,6 @@ class PH5(experiment.ExperimentGroup):
             data = data[0:samples_to_cut]
 
         # Done reading all the traces catch the last bit
-        if data is None:
-            return [Trace(np.array([]), start_fepoch, 0., 0, sample_rate,
-                          None, None, das_t, None, None, clock=clock)]
-
         trace = Trace(data,
                       trace_start_fepoch,
                       0,  # time_correction_ms
