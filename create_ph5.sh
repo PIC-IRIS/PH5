9bf18c6ca059738106701dbc6294a05674b598b5
Lan <ldam@passcal.nmt.edu>
2020-10-30 16:36:53 -0600

    change option -f for mstoph5 to option -r

--- create_ph5.sh
+++ create_ph5.sh
@@ -25,19 +25,19 @@ cd samplerate
 keftoph5 -n master_samplerate.ph5 -k ../../samplerate/all_arrays.kef -p ../samplerate
 keftoph5 -n master_samplerate.ph5 -k ../../samplerate/Expirement_SampleRate.kef -p ../samplerate
 keftoph5 -n master_samplerate.ph5 -k ../../samplerate/Receiver_SampleRate.kef -p ../samplerate
-mstoph5 -n master_samplerate.ph5 -f ../../samplerate/8H.10075..GH1.2012-08-27T23.01.00.ms
-mstoph5 -n master_samplerate.ph5 -f ../../samplerate/8H.10075..GH1.2012-08-31T17.52.00.ms
-mstoph5 -n master_samplerate.ph5 -f ../../samplerate/8H.10075..GH2.2012-08-27T23.01.00.ms
-mstoph5 -n master_samplerate.ph5 -f ../../samplerate/8H.10075..GH2.2012-08-31T17.52.00.ms
-mstoph5 -n master_samplerate.ph5 -f ../../samplerate/8H.10075..GHZ.2012-08-27T23.01.00.ms
-mstoph5 -n master_samplerate.ph5 -f ../../samplerate/8H.10075..GHZ.2012-08-31T17.52.00.ms
+mstoph5 -n master_samplerate.ph5 -r ../../samplerate/8H.10075..GH1.2012-08-27T23.01.00.ms
+mstoph5 -n master_samplerate.ph5 -r ../../samplerate/8H.10075..GH1.2012-08-31T17.52.00.ms
+mstoph5 -n master_samplerate.ph5 -r ../../samplerate/8H.10075..GH2.2012-08-27T23.01.00.ms
+mstoph5 -n master_samplerate.ph5 -r ../../samplerate/8H.10075..GH2.2012-08-31T17.52.00.ms
+mstoph5 -n master_samplerate.ph5 -r ../../samplerate/8H.10075..GHZ.2012-08-27T23.01.00.ms
+mstoph5 -n master_samplerate.ph5 -r ../../samplerate/8H.10075..GHZ.2012-08-31T17.52.00.ms
 keftoph5 -n master_samplerate.ph5 -k ../../samplerate/Das_SampleRate.kef
 mkdir error
 cd error
 keftoph5 -n master_samplerate_error.ph5 -k ../../../samplerate/all_arrays.kef -p ../error
 keftoph5 -n master_samplerate_error.ph5 -k ../../../samplerate/Expirement_SampleRate.kef -p ../error
 keftoph5 -n master_samplerate_error.ph5 -k ../../../samplerate/Receiver_SampleRate.kef -p ../error
-mstoph5 -n master_samplerate_error.ph5 -f ../../../samplerate/8H.10075..GH1.2012-08-27T23.01.00.ms
+mstoph5 -n master_samplerate_error.ph5 -r ../../../samplerate/8H.10075..GH1.2012-08-27T23.01.00.ms
 keftoph5 -n master_samplerate_error.ph5 -k ../../../samplerate/Das_SampleRate_error.kef
 cd ../../
 echo "Finished creating test PH5"
