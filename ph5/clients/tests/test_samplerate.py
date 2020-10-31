32bfbb07775015c5e9600ca31a2f698f6eaa0b2b
Lan <ldam@passcal.nmt.edu>
2020-10-31 09:18:13 -0600

    Change tearDown_sr to tearDown for it to work, change master file name to match with create_ph5.sh, make log_error to work correctly

--- ph5/clients/tests/test_samplerate.py
+++ ph5/clients/tests/test_samplerate.py
@@ -19,10 +19,10 @@ class TestPH5AvailabilitySampleRate(LogTestCase, TempDirTestCase):
         self.ph5test_path_sr = os.path.join(self.home,
                                             'ph5/test_data/ph5/samplerate')
         self.ph5_sr = ph5api.PH5(path=self.ph5test_path_sr,
-                                 nickname='master_samplerate.ph5')
+                                 nickname='master.ph5')
         self.sr_avail = ph5availability.PH5Availability(self.ph5_sr)
 
-    def tearDown_sr(self):
+    def tearDown(self):
         self.ph5_sr.close()
         super(TestPH5AvailabilitySampleRate, self).tearDown()
 
@@ -34,7 +34,6 @@ class TestPH5AvailabilitySampleRate(LogTestCase, TempDirTestCase):
             trace = ph5toms.create_trace(cut)
             if trace is not None:
                 self.assertEqual(trace[0].stats.station, '10075')
-        self.ph5_sr.close()
 
     def test_availability_samplerate(self):
         with LogCapture() as log:
@@ -43,8 +42,8 @@ class TestPH5AvailabilitySampleRate(LogTestCase, TempDirTestCase):
                                                  starttime=None,
                                                  endtime=None,
                                                  include_sample_rate=True)
-        self.assertEqual(6, len(ret))
-        self.assertIsNotNone(log)
+            self.assertEqual(6, len(ret))
+            self.assertIsNotNone(log)
         # Checks the sample rate of the test data set
         self.assertEqual(500.0, ret[0][5])
         ret2 = self.sr_avail.get_availability_extent(station='10075',
@@ -59,14 +58,15 @@ class TestPH5AvailabilitySampleRate(LogTestCase, TempDirTestCase):
                                                          channel='*',
                                                          starttime=None,
                                                          endtime=None)
-        self.assertIsNotNone(log2)
-        self.ph5_sr.close()
+            self.assertIsNotNone(log2)
 
+
+class TestPH5AvailabilitySampleRate_error(LogTestCase, TempDirTestCase):
     def test_availability_error(self):
         self.ph5_path_eror = os.path.join(self.home,
                                           'ph5/test_data/ph5/samplerate/error')
         self.ph5_sr_error = ph5api.PH5(path=self.ph5_path_eror,
-                                       nickname='master_samplerate_error.ph5')
+                                       nickname='master.ph5')
         self.avail_error = ph5availability.PH5Availability(self.ph5_sr_error)
         with LogCapture() as log_error:
             self.avail_error.get_availability(station='10075',
@@ -74,10 +74,11 @@ class TestPH5AvailabilitySampleRate(LogTestCase, TempDirTestCase):
                                               starttime=None,
                                               endtime=None,
                                               include_sample_rate=True)
-        self.assertEqual(log_error[2][2], 'DAS and Array Table sample rates do'
-                         + ' not match, DAS table sample rates'
-                         + ' do not match. Data must be'
-                         + ' updated.')
+        self.assertEqual(log_error.records[2].msg,
+                         'DAS and Array Table sample rates do'
+                         ' not match, DAS table sample rates'
+                         ' do not match. Data must be'
+                         ' updated.')
         self.ph5_sr_error.close()
 
 
