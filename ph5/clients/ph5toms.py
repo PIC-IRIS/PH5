a47b7c04866b9badac65e236560abc53145770b4
Lan <ldam@passcal.nmt.edu>
2021-01-12 15:05:00 -0700

    ph5toms: log as error for error raised from ph5api.query_das_t() for sample_rate_multiplier=0

--- ph5/clients/ph5toms.py
+++ ph5/clients/ph5toms.py
@@ -1270,7 +1270,8 @@ def main():
 
     except PH5toMSAPIError as err:
         LOGGER.error("{0}".format(err.message))
-        exit(-1)
+    except ph5api.APIError as err:
+        LOGGER.error(err.msg)
 
     ph5API_object.close()
 
