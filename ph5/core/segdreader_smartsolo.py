b6839fb57b2c16dc8d91a1906507178c69f0121f
ldam <ldam@passcal.nmt.edu>
2021-09-30 10:18:38 -0600

    modify function name from degree2decimal to getLocationDecimal

--- ph5/core/segdreader_smartsolo.py
+++ ph5/core/segdreader_smartsolo.py
@@ -137,11 +137,15 @@ class Reader ():
                     second=ghb.second_of_minute_utc)
         return t.epoch()
 
-    def degree2decimal(self, integer, fraction):
+    def getLocationDecimal(self, integer, fraction):
         """
+        OLD FORMAT: degree2decimal
         lat degree: DDMMSS.sss
         lon degree: DDDMMSS.sss
-        decimal = deg + min/60 + sec/3600
+        return decimal = deg + min/60 + sec/3600
+        NEW FORMAT: combine signed integer with string
+        integer is signed ingeter
+        return signed_integer.fraction
         """
         # NEW FORMAT: ingeger is a signed one
         degree_str = "%.3f" % (integer + fraction)
@@ -282,9 +286,10 @@ class Reader ():
             thN0.extended_receiver_point_number_integer
 
         thN3 = self.trace_headers.trace_header_N[3]
-        self.trace_headers.lat = self.degree2decimal(thN3.IGU_GPS_lat_integer,
-                                                     thN3.IGU_GPS_lat_fraction)
-        self.trace_headers.lon = self.degree2decimal(thN3.IGU_GPS_lon_integer,
+        self.trace_headers.lat = self.getLocationDecimal(
+            thN3.IGU_GPS_lat_integer, thN3.IGU_GPS_lat_fraction)
+        self.trace_headers.lon = self.getLocationDecimal(
+            thN3.IGU_GPS_lon_integer,
                                                      thN3.IGU_GPS_lon_fraction)
         self.trace_headers.ele = thN3.IGU_GPS_height
         self.trace_headers.preamp_gain_db = self.preamp_gain_db
