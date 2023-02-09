12b196a63be662095ab14466d5097cf6d3180fc5
ldam <ldam@passcal.nmt.edu>
2023-02-09 08:35:02 -0700

    kef2ph5 add option to add das.kef to kef2ph5 with a given minifile

--- ph5/utilities/kef2ph5.py
+++ ph5/utilities/kef2ph5.py
@@ -20,8 +20,22 @@ os.environ['TZ'] = 'UTC'
 time.tzset()
 
 
+class Rows_Keys(object):
+    __slots__ = ('rows', 'keys')
+
+    def __init__(self, rows=None, keys=None):
+        self.rows = rows
+        self.keys = keys
+
+    def set(self, rows=None, keys=None):
+        if rows is not None:
+            self.rows = rows
+        if keys is not None:
+            self.keys = keys
+
+
 def get_args():
-    global KEFFILE, PH5, PATH, TRACE
+    global KEFFILE, PH5, PATH, TRACE, MINIFILE
 
     parser = argparse.ArgumentParser(
                                 formatter_class=argparse.RawTextHelpFormatter)
@@ -44,12 +58,18 @@ def get_args():
                         dest="check",
                         help="Show what will be done but don't do it!")
 
+    parser.add_argument("-m", "--miniPH5_xxxxx", dest="minifile",
+                        metavar='n', default=0,
+                        help="Go with das kef file to add das table to"
+                             "a specific mini file.")
+
     args = parser.parse_args()
 
     KEFFILE = args.keffile
     PH5 = args.outfile
     PATH = args.path
     TRACE = args.check
+    MINIFILE = args.minifile
 
 
 def initializeExperiment():
@@ -61,40 +81,90 @@ def initializeExperiment():
     EX.initgroup()
 
 
-def add_references(rs):
+def openPH5(filename):
+    '''   Open PH5 file, miniPH5_xxxxx.ph5   '''
+    exrec = experiment.ExperimentGroup(nickname=filename)
+    exrec.ph5open(True)
+    exrec.initgroup()
+    return exrec
+
+
+def update_external_references():
+    '''   Update external references in master.ph5 to
+          miniPH5 files in Receivers_g    '''
+    global INDEX_T_DAS
+    LOGGER.info("Updating external references...")
+    n = 0
+    for i in INDEX_T_DAS.rows:
+        external_file = i['external_file_name_s'][2:]
+        external_path = i['hdf5_path_s']
+        target = external_file + ':' + external_path
+        external_group = external_path.split('/')[3]
+
+        #   Nuke old node
+        try:
+            group_node = EX.ph5.get_node(external_path)
+            group_node.remove()
+        except Exception as e:
+            pass
+
+        #   Re-create node
+        try:
+            EX.ph5.create_external_link(
+                '/Experiment_g/Receivers_g', external_group, target)
+            n += 1
+        except Exception as e:
+            # pass
+            LOGGER.error("{0}\n".format(e.message))
+
+    LOGGER.info("done, {0} das nodes recreated.\n".format(n))
+
+
+def add_references(rs, EXREC=None):
     '''   Add a reference for each Das_t so we can look it up later   '''
     import string
     global EX
-
     for r in rs:
         flds = string.split(r, '/')
         das = string.split(flds[3], '_')[2]
-        g = EX.ph5_g_receivers.getdas_g(das)
+        if EXREC is not None:
+            g, das_t, receiver_t, time_t = EXREC.ph5_g_receivers.newdas(
+                str(das))
+            EXREC.ph5_g_maps.newdas('Das_g_', str(das))
+        else:
+            g = EX.ph5_g_receivers.getdas_g(das)
         EX.ph5_g_receivers.setcurrent(g)
         # Set reference
         columns.add_reference(r, EX.ph5_g_receivers.current_t_das)
 
 
 def populateTables():
-    global EX, KEFFILE, TRACE
+    global EX, KEFFILE, TRACE, MINIFILE, INDEX_T_DAS
     LOGGER.info("Loading {0} into {1}.".format(KEFFILE, PH5))
     k = kefx.Kef(KEFFILE)
     k.open()
 
     while True:
         n = k.read(10000)
+
         if n == 0:
             err = "Empty kef file."
             break
+
+        EXREC = None
+
+        if MINIFILE != 0:
+            EXREC = openPH5('miniPH5_{0:05d}'.format(int(MINIFILE)))
 
         # Get Das_g references
         ret = k.strip_receiver_g()
 
         if ret:
-            add_references(ret)
+            add_references(ret, EXREC)
 
         # Make sure Array_t_xxx, Event_t_xxx, and Offset_t_aaa_sss exist
-        arrays, events, offsets = k.strip_a_e_o()
+        arrays, events, offsets, dass = k.strip_a_e_o_d()
+
         if arrays:
             for a in arrays:
                 a = a.split(':')[0]
@@ -109,12 +179,21 @@ def populateTables():
             for o in offsets:
                 o = o.split(':')[0]
                 EX.ph5_g_sorts.newOffsetSort(o)
-
-        if TRACE is True:
-            err = k.batch_update(trace=True)
+        if dass:
+            if MINIFILE == 0:
+                LOGGER.error('Require MINIFILE for Das table!!!')
+            else:
+                rows, keys = EX.ph5_g_receivers.read_index()
+                INDEX_T_DAS = Rows_Keys(rows, keys)
+                k.dass_update(EXREC)
         else:
-            err = k.batch_update()
-
+            if TRACE is True:
+                err = k.batch_update(trace=True)
+            else:
+                err = k.batch_update()
+    if dass:
+        update_external_references()
+        EXREC.ph5close()
     k.close()
     if err is True:
         LOGGER.error("There were errors! See output.")
